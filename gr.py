52add2b427dbc98c59f3a1514510e69a8bbc6a76
alexmajorov <45354290+alexmajorov@users.noreply.github.com>
2018-12-25 20:13:27 +0300

    Add files via upload
    untitled.ui is replaced with layout.py

--- gr.py
+++ gr.py
@@ -1,64 +1,106 @@
+##Проект сделан Майоровым Александром
+##Математическая утилита, стоящая графики, с возможностью использовать параметры
+##Также строятся графики производной и первообразной
+
 import sys
-from PyQt5 import uic
+from layout import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from sympy import *
-from math import *
 
 
 def derivative(func):
-    x = Symbol("x")    
-    return eval(func.replace("x", "Symbol('x')")).diff(x)
+    """Определение производной"""
+
+    x = Symbol("x")
+    return str(eval(func.replace("x", "Symbol('x')")).diff(x))
+
 
 def antiderivative(func):
+    """Определение первообразной"""
+
     return integrate(func).__repr__()
 
+
 def frange(start, stop, step):
+    """Аналог range, но с возможность использовать float"""
+
     i = start
     while i < stop:
         yield i
         i += step
 
-class MyWidget(QMainWindow):
+
+class MyWidget(QMainWindow, Ui_MainWindow):
+    """Главный класс окна"""
+
     def __init__(self):
         super().__init__()
-        uic.loadUi('untitled.ui', self)
-
-        self.pushButton.clicked.connect(self.run)
-
-    def run(self):
-        self.graphic.clear()
-        self.s = int(self.start.text())
-        self.t = int(self.to.text())
-        #self.build(self.func.text())
-        print("Function:", self.func.text())
-        #self.build(derivative(self.func.text()))
-        print("Derivative:", derivative(self.func.text()))
-        #self.build(antiderivative(self.func.text()))
-        print("Antiderivative:", antiderivative(self.func.text()))
-        
-        
-        
-        
-    def build(self, func):
+        self.setupUi(self)
         try:
-            print(func)
-            self.f = []
-            for i in frange(float(self.s), float(self.t + 1), 0.1):   
-                x = i
-                try:
-                    self.f.append(eval(func))
-                except Exception:
-                    self.f.append(0)
-            
-            self.graphic.plot([i for i in frange(float(self.s), float(self.t + 1), 0.1)],
-                              self.f, pen='w')
-            print(self.f, [i for i in frange(float(self.s), float(self.t + 1), 0.1)])
-            print(func)
+            self.pushButton.clicked.connect(self.run)
         except Exception as e:
             print(str(e))
 
+    def run(self):
+        """Главные вычисления"""
+
+        try:
+            
+            """Работа с параметрами"""
+            
+            self.parameters = self.params.text().split(",")
+            
+            for i in range(len(self.parameters)):
+                self.parameters[i] = self.parameters[i].split("=")
+
+            self.function = self.func.text()
+            if [''] not in self.parameters:
+                for i in self.parameters:
+                    self.function = self.function.replace(i[0], i[1])
+                
+            self.derivative_output.clear()
+            self.antiderivative_output.clear()
+
+            self.s = int(self.start.text())
+            self.t = int(self.to.text())
+
+            """Простроение графиков"""
+
+            self.build(self.function, self.graphic)
+            self.build(derivative(self.function), self.deriv_gr)
+            self.build(antiderivative(self.function), self.antideriv_gr)
+
+            """Вывод текста в окне"""
+
+            self.derivative_output.appendPlainText("f′(x):" + derivative(self.function))
+            self.antiderivative_output.appendPlainText("F(x):" + antiderivative(self.function))
+
+        except Exception as e:
+            print(str(e))
+
+    def build(self, func, pl):
+        """Функция, строящая графики"""
+
+        pl.clear()
+        self.values = []
+
+        for i in frange(float(self.s), float(self.t + 1), 0.5):
+            x = i
+            try:
+                self.values.append(float(eval(func)))
+            except Exception as e:
+                self.values.append(0)
+                print(str(e))
+
+        pl.plot([i for i in frange(float(self.s), float(self.t + 1), 0.5)],
+                self.values, pen='w')
+
+
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
-    sys.exit(app.exec_())
+    try:
+        app = QApplication(sys.argv)
+        ex = MyWidget()
+        ex.show()
+        sys.exit(app.exec_())
+    except Exception as e:
+        print(str(e))
